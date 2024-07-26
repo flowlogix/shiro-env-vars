23b3fa20eb8df08ec051c65610a6173167ab7eae
lprimak <lenny@flowlogix.com>
2024-07-26 17:16:44 -0500

    added env variable setup

--- src/test/java/com/flowlogix/reproducers/EnvVarIT.java
+++ src/test/java/com/flowlogix/reproducers/EnvVarIT.java
@@ -4,21 +4,15 @@ import com.flowlogix.util.ShrinkWrapManipulator;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit5.ArquillianExtension;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(ArquillianExtension.class)
 public class EnvVarIT {
-    @BeforeEach
-    void setupEnvironment() {
-        System.setProperty("my_variable_xxx", "hello");
-    }
-
     @Test
     void envVar() {
-        assertThat(System.getProperty("my_variable_xxx")).isEqualTo("hello");
+        assertThat(System.getenv("my_env_var")).isEqualTo("my_value");
     }
 
     @Deployment
